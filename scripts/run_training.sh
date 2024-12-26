fbe93cbf8ad31bd782e3c7df0186d1378c58d5fb
Francisco Manuel Ribeiro <fmribeiro@ctm-login-02.inesctec.pt>
2024-12-26 14:48:48 +0000

    train with full data

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH --partition=debug_8gb  # Partition where the job will be run. Check with "$ sinfo".
-#SBATCH --qos=debug_8g        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --partition=gpu_min8gb  # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min8gb        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
 #SBATCH --job-name=debug_csi2har    # Job name
 #SBATCH -o debug_csi2har.out       # File containing STDOUT output
 #SBATCH -e debug_csi2har.err       # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
-# python3 train_model.py --test_split 0.5 --val_split 0.5 --batch_size 24
+python3 train_model.py --test_split 0.8 --val_split 0.2 --test_split 0 --batch_size 24 --dataset_root /nas-ctm01/datasets/public/CSI-HAR/Dataset/UT_HAR_OG
