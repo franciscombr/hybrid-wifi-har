1b73b0bf5b482f0b7fda60af9a6b57320353a810
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2024-12-27 22:46:14 +0000

    corrected model name

--- scripts/train_model_arch02.py
+++ scripts/train_model_arch02.py
@@ -146,6 +146,7 @@ def main(args):
             "num_heads": 4,
             "num_encoder_layers": 3,
             "num_classes": 8,
+            "arch": 02
         },
         name=f"CSI2HAR_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
     )
@@ -267,7 +268,7 @@ def main(args):
         print('    >> EPOCH {}/{} \t train loss {:.6f} \t train_acc {:.6f} \t val loss {:.6f} \t val acc {:.6f}'.format(epoch + 1, num_epochs, train_loss, train_acc, val_loss, val_acc))
 
         # Save checkpoint at the end of each epoch
-        model_version = "arch01_v1_002"
+        model_version = "arch02_v1_001"
         if epoch % 10 == 0:
             save_checkpoint(model, optimizer, epoch, val_loss, checkpoint_dir="../results/models/checkpoints/", name_model=model_version)
 
@@ -276,7 +277,7 @@ def main(args):
         # Saving the model.
     save_model = True
     if save_model:
-        model_version = "arch01_v1_002"
+        model_version = "arch01_v2_001"
         day_model = "091024"
         torch.save(model.state_dict(), f"../results/models/hybrid_har_model_{day_model}{model_version}_ep{num_epochs}_lr{learnig_rate}_{optimizer_name}.pth")
         print(f" >> Model saved with name: hybrid_har_model_{day_model}{model_version}_ep{num_epochs}_lr{learnig_rate}_{optimizer_name}.pth")
