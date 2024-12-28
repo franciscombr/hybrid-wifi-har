e4897f8e9c46622bc5745dd7c3787e09b89ed6a4
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2024-12-28 12:45:26 +0000

    count=10 for number of agents

--- scripts/train.py
+++ scripts/train.py
@@ -4,4 +4,4 @@ from train_utils_arch02 import wandb_train_sweep
 if __name__ == '__main__':
     sweep_id = "wbs3i0dl" 
     # Run the sweep
-    wandb.agent(sweep_id, function=wandb_train_sweep, project = "HAR-CSI2")
+    wandb.agent(sweep_id, function=wandb_train_sweep, project = "HAR-CSI2",count=10)
