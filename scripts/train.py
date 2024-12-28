6cbb29c30b6279998166b36f8edb1ccbba902028
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2024-12-28 12:48:33 +0000

    add sweep id for new sweep

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,6 @@ import wandb
 from train_utils_arch02 import wandb_train_sweep 
 
 if __name__ == '__main__':
-    sweep_id = "wbs3i0dl" 
+    sweep_id = "26rs1x76"
     # Run the sweep
     wandb.agent(sweep_id, function=wandb_train_sweep, project = "HAR-CSI2",count=10)
