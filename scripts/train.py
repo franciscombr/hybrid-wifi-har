ed6340695de6fc1bb817ead6b33909d673300e19
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2024-12-29 10:41:05 +0000

    train with csi ratio dataset

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,6 @@ import wandb
 from train_utils_arch02 import wandb_train_sweep 
 
 if __name__ == '__main__':
-    sweep_id = "71grhpeu"
+    sweep_id = "1yu1qa3r"
     # Run the sweep
     wandb.agent(sweep_id, function=wandb_train_sweep, project = "HAR-CSI2",count=10)
