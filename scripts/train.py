2015dbb1104a8bc0db6fcc0b36dd47c6ffe7ba90
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-01-02 13:59:51 +0000

    was missing changing the sweep id

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,6 @@ import wandb
 from src.lstm.train_utils_lstm import wandb_train_sweep 
 
 if __name__ == '__main__':
-    sweep_id = "y4cev91k"
+    sweep_id = "51au0vkl"
     # Run the sweep
     wandb.agent(sweep_id, function=wandb_train_sweep, project = "HAR-CSI2",count=10)
