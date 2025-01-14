0be7ca78221f282ca969c50970435f9baca582a7
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-01-14 21:51:47 +0000

    multiple sweeps per batch

---
+++ scripts/run_sweeps_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min8gb  # Partition where the job will be run. Check with "$ sinfo".
+#SBATCH --qos=gpu_min8gb        # QoS level. Must match the partition name. External users must add the suffix "_ext". Check with "$sacctmgr show qos".
+#SBATCH --job-name=sweep_1    # Job name
+#SBATCH -o logs/sweep_1.out       # File containing STDOUT output
+#SBATCH -e logs/sweep_1.err       # File containing STDERR output. If ommited, use STDOUT.
+
+# Commands / scripts to run (e.g., python3 train.py)
+python3 train_csi4har.py
+python3 train_resnet.py
+python3 train_lstm.py
+python3 train_cnn_lstm.py
