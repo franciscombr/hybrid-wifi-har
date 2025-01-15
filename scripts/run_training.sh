a035ed47b4bd853d7144de89e89ef4988627420e
francisco.m.ribeiro <francisco.m.ribeiro@inesctec.pt>
2025-01-15 18:05:49 +0000

    was missing norm

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -7,4 +7,4 @@
 #SBATCH -e debug_csi2har.err       # File containing STDERR output. If ommited, use STDOUT.
 
 # Commands / scripts to run (e.g., python3 train.py)
-python3 ../src/csi2har/train_utils_arch01.py --learning_rate 0.0001 --num_epochs 50 --test_split 0.2 --val_split 0.2  --batch_size_train 16 --batch_size_val 8 --embedding_dim 256 --num_encoder_layers 4 --dataset_root /nas-ctm01/datasets/public/CSI-HAR/Dataset/UT_HAR_CAL_PHASE 
+python3 ../src/csi2har/train_utils_arch01.py --normalize --learning_rate 0.0001 --num_epochs 50 --test_split 0.2 --val_split 0.2  --batch_size_train 16 --batch_size_val 8 --embedding_dim 256 --num_encoder_layers 4 --dataset_root /nas-ctm01/datasets/public/CSI-HAR/Dataset/UT_HAR_CAL_PHASE 
